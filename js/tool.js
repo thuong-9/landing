cf3cd8993b4dafa96f038b7f2a1480aa6294b527
Trang pham <trangpd204@gmail.com>
2025-11-19 15:17:04 +0700

    themsuatutirial

---
+++ js/tool.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+  $('.single-item').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    dots: true,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
+});
